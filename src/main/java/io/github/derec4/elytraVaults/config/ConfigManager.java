8b5d149b62e61c0c066b03d17d8180870ae0dae9
Derek <derex1987@gmail.com>
2025-12-18 17:11:23 -0600

    feat: pause

--- src/main/java/io/github/derec4/elytraVaults/config/ConfigManager.java
+++ src/main/java/io/github/derec4/elytraVaults/config/ConfigManager.java
@@ -1,4 +1,47 @@
 package io.github.derec4.elytraVaults.config;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
 public class ConfigManager {
+    private final JavaPlugin plugin;
+    private FileConfiguration config;
+
+    private static final String DETECTION_RADIUS = "detection-radius";
+    private static final String ENABLE_TEXT_DISPLAY = "enable-text-display";
+    private static final String TEST_MODE = "test-mode";
+
+    public ConfigManager(JavaPlugin plugin) {
+        this.plugin = plugin;
+        loadConfig();
+    }
+
+    private void loadConfig() {
+        plugin.saveDefaultConfig();
+        config = plugin.getConfig();
+
+        config.addDefault(DETECTION_RADIUS, 10);
+        config.addDefault(ENABLE_TEXT_DISPLAY, true);
+        config.addDefault(TEST_MODE, true);
+        config.options().copyDefaults(true);
+
+        plugin.saveConfig();
+    }
+
+    public int getDetectionRadius() {
+        return config.getInt(DETECTION_RADIUS, 10);
+    }
+
+    public boolean isTextDisplayEnabled() {
+        return config.getBoolean(ENABLE_TEXT_DISPLAY, true);
+    }
+
+    public boolean isTestMode() {
+        return config.getBoolean(TEST_MODE, true);
+    }
+
+    public void reload() {
+        plugin.reloadConfig();
+        config = plugin.getConfig();
+    }
 }
